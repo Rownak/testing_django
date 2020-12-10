d3d98ed93a7bea7aa263a2d59e605aa1da2a69d8
Ubuntu <ubuntu@ip-172-31-39-116.us-east-2.compute.internal>
2020-12-10 09:43:35 +0000

    update the return array. Shape of resulting array is changed into 2d array

--- django_test/users/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersConfig(AppConfig):
-    name = 'users'
-
-    def ready(self):
-    	#django documentation recommend to add this for signal
-    	import users.signals
